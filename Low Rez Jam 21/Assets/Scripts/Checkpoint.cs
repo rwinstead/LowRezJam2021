3b02061582571405845dcfec5951a5e53c45341f
foxencore <foxencore@users.noreply.github.com>
2021-08-04 15:43:55 -0500

    added checkpoint and campfire animations

---
+++ Low Rez Jam 21/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator checkpointAnim;
+    public bool hasTriggered = false;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Trigger animation");
+            checkpointAnim.SetTrigger("checkpoint_reached");
+            hasTriggered = true;
+        }
+    }
+}
