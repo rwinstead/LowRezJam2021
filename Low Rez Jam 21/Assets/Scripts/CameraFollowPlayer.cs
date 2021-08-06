a4df81ae065c52ba0839dd40c1d3c8fdf8db0afe
foxencore <foxencore@users.noreply.github.com>
2021-08-06 17:19:08 -0500

    changed camera to move vertically

--- Low Rez Jam 21/Assets/Scripts/CameraFollowPlayer.cs
+++ Low Rez Jam 21/Assets/Scripts/CameraFollowPlayer.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
     public Vector3 offset = Vector3.zero;
+
+    private float platformOffset = 0f;
+
+    private MovementController Controller;
+
+    
+    public float transitionSpeedY = .4f;
+    
+    private Vector2 targetY;
+    private Vector2 targetYThisUpdate;
 
     
     void Start()
     {
+        Controller = player.GetComponent<MovementController>();
         offset.z = -8f;
     }
     
     void Update()
     {
-        transform.position = new Vector3(player.position.x + offset.x, 0 + offset.y, offset.z); 
+        if (Controller.m_Grounded)
+        {
+            platformOffset = player.transform.position.y;
+        }
+
+        targetY = new Vector2(transform.position.x, platformOffset + offset.y);
+        targetYThisUpdate = Vector2.MoveTowards(transform.position, targetY, (transitionSpeedY * Time.deltaTime));
+
+        transform.position = new Vector3(player.transform.position.x + offset.x,targetYThisUpdate.y, offset.z);
     }
 }
