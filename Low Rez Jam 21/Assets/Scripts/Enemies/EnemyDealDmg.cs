f632d0a1588f1328e90202642898d41488312ca5
rwinstead6 <62908528+rwinstead6@users.noreply.github.com>
2021-08-07 17:26:09 -0400

    first attempt at combat knockback and dmg flashing

--- Low Rez Jam 21/Assets/Scripts/Enemies/EnemyDealDmg.cs
+++ Low Rez Jam 21/Assets/Scripts/Enemies/EnemyDealDmg.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class EnemyDealDmg : MonoBehaviour
 {
 
+    public float knockbackForce;
+
+    public GameObject parent;
+
+    private Vector2 direction;
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<Health>().takeDamage();
+            GameObject player = collision.gameObject;
+            player.GetComponent<Health>().takeDamage();
+
+            direction = (player.transform.position - parent.transform.position).normalized;
+            player.gameObject.GetComponent<Rigidbody2D>().AddForce(direction * knockbackForce, ForceMode2D.Impulse);
+            parent.GetComponent<Rigidbody2D>().AddForce(-direction * knockbackForce, ForceMode2D.Impulse);
         }
     }
 
--- Low Rez Jam 21/Assets/Scripts/Player/Health.cs
+++ Low Rez Jam 21/Assets/Scripts/Player/Health.cs
@@ -10,12 +10,16 @@ public class Health : MonoBehaviour
     public float invincibleTime = 1f;
     public float lastTimeDamaged = -50f;
 
+    SpriteRenderer spriteRend;
+    bool blinkingWhite = false;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void takeDamage(int amt = 1)
@@ -23,6 +27,7 @@ public class Health : MonoBehaviour
         
         if(Time.time - lastTimeDamaged > invincibleTime)
         {
+            StartCoroutine("FlashWhite");
             currentHealth = (currentHealth - amt);
             Debug.Log("Ouch. Took " + amt + " Damage. Current Health: " + currentHealth);
             lastTimeDamaged = Time.time;
@@ -38,6 +43,18 @@ public class Health : MonoBehaviour
         {
             // die
      
+        }
+    }
+
+    public IEnumerator FlashWhite()
+    {
+        if (!blinkingWhite)
+        {
+            blinkingWhite = true;
+            spriteRend.material.SetFloat("_FlashAmount", .75f);
+            yield return new WaitForSeconds(.2f);
+            spriteRend.material.SetFloat("_FlashAmount", 0);
+            blinkingWhite = false;
         }
     }
     
