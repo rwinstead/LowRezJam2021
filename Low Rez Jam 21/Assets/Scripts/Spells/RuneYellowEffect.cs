1363b5234ba027a6c261a20f95b18265e72fdd72
rwinstead6 <62908528+rwinstead6@users.noreply.github.com>
2021-08-11 14:47:26 -0400

    lightning spell anim working

--- Low Rez Jam 21/Assets/Scripts/Spells/RuneYellowEffect.cs
+++ Low Rez Jam 21/Assets/Scripts/Spells/RuneYellowEffect.cs
@@ -6,9 +6,23 @@ public class RuneYellowEffect : MonoBehaviour
 {
     public CircleCollider2D col;
 
+    public Transform[] lightningBolts = new Transform[4];
+
+    int spot1;
+    int spot2;
+
     public void ShockEnemies()
     {
-        col.enabled = true;
+        spot1 = -1;
+        spot2 = -1;
+        while (spot1 == spot2)
+        {
+            spot1 = Random.Range(0, 4);
+            spot2 = Random.Range(0, 4);
+        }
+
+        lightningBolts[spot1].gameObject.SetActive(true);
+        lightningBolts[spot2].gameObject.SetActive(true);
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
