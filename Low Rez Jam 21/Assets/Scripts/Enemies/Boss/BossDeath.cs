1c256f5130e3ca7fcbd4775a2db5d7068e075632
Josh Todd <foxencore@users.noreply.github.com>
2021-08-14 17:27:25 -0500

    Game waits 2 seconds after boss death to trigger victory scene

---
+++ Low Rez Jam 21/Assets/Scripts/Enemies/Boss/BossDeath.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossDeath : MonoBehaviour
+{
+
+	public int waitSecondsOnDeath = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     	EnemyHitManager.bossDeath += bossDeathHandler;   
+    }
+
+    void bossDeathHandler()
+    {
+        StartCoroutine(waitOnBossDeath());
+    }
+
+    public IEnumerator waitOnBossDeath()
+    {
+        Debug.Log("Boss Dead, Waiting for Seconds "+waitSecondsOnDeath);
+        yield return new WaitForSeconds(waitSecondsOnDeath);
+        Debug.Log("LoadingScene Victory");
+        SceneManager.LoadScene("Victory");
+    }
+
+}
